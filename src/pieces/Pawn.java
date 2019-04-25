4eb99b8b523e6e600374baafc65a553b2f0fe6fc
Khazra-Parssa-216147936 <46363769+Khazra-Parssa-216147936@users.noreply.github.com>
2019-04-25 00:53:11 -0400

    Update Pawn.java

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -10,25 +10,25 @@ import javafx.scene.image.ImageView;
 public class Pawn extends Piece
 {
 	private String url;
-	
+
 	public Pawn(String col)
 	{
 		super(Piece.PAWN, col);
 		url = "/pieces/chess sprites/"+col+"Pawn"+".png";
-		
+
 		super.setName("Pawn");
 		super.setImage(new ImageView(new Image(url))) ;
 	}
-	
+
 	public ArrayList<int[]> generateMoveSet(Tile t)	
 	{
 		ArrayList<int[]> moveSet = super.getMoveSet();
 		moveSet.clear();
-		
+
 		int row = t.getCoordinates()[0], col = t.getCoordinates()[1];
 		Piece p = t.getPiece();
-		
-		
+
+
 		/*
 		 * if it is white it moves up, else moves down
 		 * first turn it can move up 2 spaces
@@ -41,60 +41,70 @@ public class Pawn extends Piece
 		int limits=1;
 		if(super.moveCount ==0)
 			limits++;
-		
+
 
 		//move forward 1 or 2
 		boolean blocked =false;
-		
+
 		for(int i=0; i< limits && !blocked; i++)
 		{
-			if(row>0 && p.col.equals("white") && !Board.grid[row-1-i][col].isOccupied())
+
+			if(p.col.equals("white"))
 			{
-				moveSet.add(new int[] {row-(i+1), col});
+				//check if its blocked
+				if(row==0 || Board.grid[row-1-i][col].isOccupied())
+					blocked = true;
+				else
+					moveSet.add(new int[] {row-(i+1), col});
+				
 			}
-			else if (row>0 && p.col.equals("white") && Board.grid[row-1-i][col].isOccupied())
-				blocked = true;
-			
-			if(row<8 && p.col.equals("black") && !Board.grid[row+1+i][col].isOccupied())
+			else
 			{
-				moveSet.add(new int[] {row+(i+1), col});
+				//check if its blocked
+				if(row==7 || Board.grid[row+1+i][col].isOccupied())
+					blocked = true;
+				else
+					moveSet.add(new int[] {row+(i+1), col});
 			}
-			else if(row<8 && p.col.equals("black") && Board.grid[row+1+i][col].isOccupied())
-				blocked = true;
-			
+
 		}
 
 		//killing move set
-		if(p.col.equals("white"))
-		{
-			
-			if(row>0 &&col < 7 && Board.grid[row-1][col+1].isOccupied() && Board.grid[row-1][col+1].getPiece().getCol().equals("black") )
-			{
-				moveSet.add(new int[]{row-1, col+1});
-			}
-			if(row>0 && col>0 && Board.grid[row-1][col-1].isOccupied() && Board.grid[row-1][col-1].getPiece().getCol().equals("black"))
-			{
-				moveSet.add(new int[]{row-1, col-1});
-			}
+		int x=0, y=0;
+		if(row >0 && col < 7 && row <7 && col >0)
 
-		}
-		else
-		{
-			if(row>0 &&col < 7 && Board.grid[row+1][col+1].isOccupied() && Board.grid[row+1][col+1].getPiece().getCol().equals("white"))
+			for(int i = 0; i<4; i++)
 			{
-				moveSet.add(new int[]{row+1, col+1});
+				if(i ==0)
+				{
+					x=1; y = 1;
+				}
+				else if(i == 1)
+				{
+					x=1; y =-1;
+				}
+				else if(i == 2)
+				{
+					x=-1; y= 1;
+				}
+				else
+				{
+					x=-1; y=-1;
+				}
+				if( (p.col.equals("white") && (i == 3 || i==2)) || (p.col.equals("black") && (i == 0 || i==1)) )	
+				{
+					if(Board.grid[row+x][col+y].isOccupied() && !p.getCol().equals(Board.grid[row+x][col+y].getPiece().getCol()))
+						moveSet.add(new int[] {row+x, col+y});
+				}
+				
 			}
-			if(row>0 && col>0 && Board.grid[row+1][col-1].isOccupied() && Board.grid[row+1][col-1].getPiece().getCol().equals("white"))
-			{
-				moveSet.add( new int[]{row+1, col-1});
-			}
-		}
 		
-		
+
+
 		return moveSet;
 	}
-	
-	
-	
-	
+
+
+
+
 }
