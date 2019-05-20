6b58085cbf7724adeb58676e09b02a76a0257f5c
ameroft <emmanuelsogelola@gmail.com>
2019-05-20 12:51:34 -0400

    weird changes

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-
+import Game.Board;
 import Game.Game;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 
 
-public class Main extends Application 
+public class Main extends Application
 {
 	public void start(Stage primaryStage) 
 	{
@@ -22,5 +22,7 @@ public class Main extends Application
 	{
 		launch(args);
 	}
-
 }
+
+
+
